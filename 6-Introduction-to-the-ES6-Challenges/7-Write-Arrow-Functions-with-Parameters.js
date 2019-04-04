3fa689e49548395d7d3ade4fa71784f839a331d5
Flanneryj <42059803+Flanneryj@users.noreply.github.com>
2019-04-04 15:18:13 -0600

    Update 7-Write-Arrow-Functions-with-Parameters.js

---
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -0,0 +1,7 @@
+* Arrow functions allow you to pass arguments through
+You can pass more than one argument at a time!
+ */
+ const myConcat = (arr1, arr2) => arr1.concat(arr2);
+
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
