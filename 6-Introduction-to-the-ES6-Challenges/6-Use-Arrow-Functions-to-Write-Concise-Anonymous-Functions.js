926950c15ce31812f3c61f790786a7865b48a2e9
Flanneryj <42059803+Flanneryj@users.noreply.github.com>
2019-04-04 15:17:01 -0600

    Update 6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js

---
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -0,0 +1,7 @@
+/* Using the arrow function syntax I was able to declare an anonymous function in a shorthand context
+We often don't need to name our functions, especially when passing a function as an argument to another function
+* considered inline functions.
+*/
+
+// new code
+const magic = () => new Date()
