a7e6367c2573b0065d7567f1451fbc377a4a3d6a
Flanneryj <42059803+Flanneryj@users.noreply.github.com>
2019-04-04 15:27:31 -0600

    Update 25-Create-an-Export-Fallback-with-export-default.js

---
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -0,0 +1,7 @@
+/*> (Export Default) use this syntax if only one value is being exported from a file
+export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file
+
+*/
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
