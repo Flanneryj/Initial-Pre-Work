dbc9618cae71e91402e5f4e6c0d6431f5ed25ff5
Flanneryj <42059803+Flanneryj@users.noreply.github.com>
2019-04-04 15:26:39 -0600

    Update 23-Use-export-to-Reuse-a-Code-Block.js

---
+++ 6-Introduction-to-the-ES6-Challenges/23-Use-export-to-Reuse-a-Code-Block.js
@@ -0,0 +1,9 @@
+/*> (Import) can be leveraged to import small amounts of code from large files
+When we want some code - a function, or a variable - to be usable in another file,
+we must export it in order to import it into another file
+export is a non-browser feature just like import
+(named export) can import any code we export into another file with the import syntax
+*/
+
+export const foo = "bar";
+export const bar = "foo";
